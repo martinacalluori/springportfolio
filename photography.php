622c6253dba3b6fdd0a90cc337e111a6055c1919
Martina Calluori <martinacalluori@Martinas-MacBook-Pro-2.local>
2017-06-13 13:09:25 -0400

    Include

--- photography.php
+++ photography.php
@@ -2,51 +2,7 @@
 
 <?php require_once 'includes/config.php'; ?>
 <?php require_once 'includes/header.php'; ?>
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Photography</title>
 
-	<link rel="stylesheet" href="css/normalize.css">
-	<link rel="stylesheet" href="css/main.css">
-	<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-<link href="https://fonts.googleapis.com/css?family=Josefin+Slab|Open+Sans|Raleway|Rosario" rel="stylesheet">
- <noscript>
-    <style>
-      .visually-hidden {
-        clip: none;
-        height: auto;
-        display: block !important;
-        width: auto;
-        overflow: auto;
-        position: static;
-      }
-    </style>
-  </noscript>
-
-</head>
-<body>
-
-<div class="header">
-	<div class="row-nav">
-		<header>
-			<nav>
-
-		        <img src="images/burger.png" alt="hamburger menu" width="50" height="50" class="ham-menu">
-
-				<ul id="nav-list" class="visually-hidden">
-					 <li><a href="index.html">Home</a> </li>
-					 <li><a href="photography.php">Photography</a>
-					 </li>
-					 <li><a href="resume.html">Resume</a></li>
-					<li><a href="contact.html"> Contact</a> </li>
-				</ul>
-
-			</nav>
-		</header>
-	</div>
-</div> -->
 
 <main>
 	 <ul class="photo-container">
@@ -60,17 +16,12 @@
 							die ("Database query failed");
 						}
 
-						// $bottle = $row['band'];
-
 						while ($row = mysqli_fetch_assoc($result)){
 							echo '<a href="fullsize.php?id=';
 							echo $row['id'];
 							echo '&band=';
-							// echo $row['band'];
 							echo urlencode($row['band']);
 							echo '"> ';
-							// echo "<li class='photo'";
-							// echo "'>";
 							echo '<img src="';
 							echo $row['filepath'];
 							echo '"';
